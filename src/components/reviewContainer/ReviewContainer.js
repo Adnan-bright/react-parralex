654e8ee3b28017430ed66d4b96043a779e8f59a2
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 <connect.adnan.guru@gmail.com>
2022-03-30 11:35:55 +0500

    review slider added in contact section

--- src/components/reviewContainer/ReviewContainer.js
+++ src/components/reviewContainer/ReviewContainer.js
@@ -1,25 +1,61 @@
 import React from 'react'
 import * as style from "../../styles/components/reviewContainer.module.css"
+import Slider from "react-slick";
+
 function ReviewContainer() {
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+    };
+    const items = [
+        {
+            ratings: '4.7',
+            feedback: "“There are a lot of benefits to working with MAG-RAW, and that’s why I’ll continue to go back to them.”",
+            reference: "Solutions Advisor, IT Business &#38; Solutions Firm"
+        },
+        {
+            ratings: '5.0',
+            feedback: "“There are a lot of benefits to working with MAG-RAW, and that’s why I’ll continue to go back to them.”",
+            reference: "lorem ipsum"
+        },
+    ]
+
     return (
         <div
             className={style.main}
         >
             <div className={style.mainSection}>
                 <img className={style.mainImg} src='/images/contact/singleBox.png' />
-                <div className={style.dataContainer}>
-                    <div className={style.revCont}>
-                        <p className={style.revText}>5.0</p>
-                        <img
-                            className={style.starIcon}
-                            src='/images/contact/star.webp' />
-                    </div>
-                    <center>
-                        <p className={style.recDesc}>“There are a lot of benefits to working with MAG-RAW, and that’s why I’ll continue to go back to them.”</p>
-                    </center>
-                    <center>
-                        <p className={style.lowerText}>Solutions Advisor, IT Business &#38; Solutions Firm</p>
-                    </center>
+                <div className={style.sliderContainer}>
+                    <Slider
+                        {...settings}
+                        className={style.slider}
+                    >
+                        {
+                            items.map((item, index) => {
+                                return <div key={index} className={style.dataContainer}>
+                                    <div className={style.revCont}>
+                                        <p className={style.revText}>{item.ratings}</p>
+                                        <img
+                                            className={style.starIcon}
+                                            src='/images/contact/star.webp' />
+                                    </div>
+                                    <center>
+                                        <p className={style.recDesc}>{item.feedback}</p>
+                                    </center>
+                                    <center>
+                                        <p className={style.lowerText}>{item.reference}</p>
+                                    </center>
+                                </div>
+                            })
+                        }
+
+                    </Slider>
                 </div>
             </div>
         </div>
