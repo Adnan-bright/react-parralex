14facb74bcd0264199aed24a81c12f8cc86c90f9
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 <connect.adnan.guru@gmail.com>
2022-04-06 17:13:40 +0500

    test23

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,8 @@
 
 import React, { useState } from "react";
-import { Provider } from 'react-redux'
 import './src/styles/global.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import store from './src/config/Store';
 export const wrapPageElement = ({ element, props }) => {
   return <div>{element}</div>
 };
--- src/config/Store.js
+++ src/config/Store.js
@@ -1,6 +1,6 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "../store/rootReducer";
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// import { applyMiddleware, createStore } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "../store/rootReducer";
+// const store = createStore(rootReducer, applyMiddleware(thunk));
 
-export default store;
+// export default store;
