5f2ea82161bf1da3296ed8aeaa49d268646f99a4
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 <connect.adnan.guru@gmail.com>
2022-04-11 16:54:49 +0500

    atest

--- src/components/storyLayout/StoryLayout.js
+++ src/components/storyLayout/StoryLayout.js
@@ -57,7 +57,7 @@ function StoryLayout({ pageContext, data }) {
                     </div>
                     <h1 className={style.bannerHeading}>
                         {storyData.storyTitle}
-                        <picture>
+                        <picture >
                             <source srcset="/images/common/dummy.png" media="(min-width: 800px)" />
                                 <img src="/images/common/film.png" alt="" />
                         </picture>
