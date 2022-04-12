b4f9859f1e0b0b4af5cf0196605ecd1fa8ef49ca
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 <connect.adnan.guru@gmail.com>
2022-04-12 13:00:07 +0500

    all stories header section err resolved

--- src/pages/allStories.js
+++ src/pages/allStories.js
@@ -1,9 +1,30 @@
+import gsap from 'gsap';
 import React from 'react'
+import Layout from '../components/layout/Layout';
 import StoriesViewAll from "../components/storiesViewAll/StoriesViewAll";
-
+import UseWindowDimension from '../components/hooks/useWindowDimension';
+import { navigate } from 'gatsby';
 function AllStories() {
+
+  const { width } = UseWindowDimension()
+
+  const handleClick = (id) => {
+    const value = id * 700
+    if (value) {
+      navigate('/')
+      gsap.to(window, {
+        scrollTo: value,
+        duration: .5,
+      })
+    } else {
+      navigate('/')
+    }
+
+  }
   return (
-    <div><StoriesViewAll /></div>
+    <Layout isMobile={width >= 800 ? false : true}  isVertical={true} onNavClick={handleClick}>
+      <div><StoriesViewAll /></div>
+    </Layout>
   )
 }
 
