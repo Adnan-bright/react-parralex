6e3e39f8ffbcfe0bcdc604017f0e26f3bc96b790
Adnan <connect.adnan.guru@gmail.com>
2022-05-14 11:55:56 +0500

    favico err resolved

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "static/images/common/favico.jpeg",
+        icon: "static/images/common/favico.png",
       },
     },
     {
