778b15374c8fcfa32a996c9aae13dfe1613a3212
Adnan <connect.adnan.guru@gmail.com>
2022-05-14 11:52:32 +0500

    moved tracking id to env

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-228684445-3",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
       },
     },
     {
