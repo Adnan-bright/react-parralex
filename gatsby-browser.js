a9423d6de8f0262287fb5d4ff8a61ef52f02af25
Adnan <connect.adnan.guru@gmail.com>
2022-05-27 18:46:52 +0500

    link color changed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
 import Provider from "./src/components/Provider/Provider";
-import '@wordpress/block-library/build-style/style.css'
-
+import '@wordpress/block-library/build-style/style.css';
 export const wrapRootElement = Provider;
